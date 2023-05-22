2a4ce1366575e33ac6ac84f5d117d61f481b346f
Kévin <kevin.lecalvez3@gmail.com>
2023-05-22 22:14:14 +0200

    Ajout onglet + grid avec images

--- front/myApp/src/app/app.module.ts
+++ front/myApp/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatNativeDateModule} from '@angular/material/core';
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,14 +20,18 @@ import { AccueilComponent } from './accueil/accueil.component';
     AppComponent,
     NavbarComponent,
     RumeursComponent,
-    AccueilComponent
+    AccueilComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatNativeDateModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule,    
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
